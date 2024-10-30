df837e658e6468c166cf4051f0159015cf6f6df0
Travis Ogle <travis.ogle@datacom.com>
2024-10-31 10:45:27 +1300

    apache server deployed to vm

---
+++ customdata.tpl
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Update the package list and upgrade existing packages
+
+sudo apt update && sudo apt upgrade -y
+
+# Script itself can run in priviliged, or run sudo / so I don't have to do it for each line
+
+# Install Apache2 web server
+
+sudo apt install -y apache2
+ 
+# Start the Apache service
+
+sudo systemctl start apache2
+ 
+# Enable Apache to start on boot
+
+sudo systemctl enable apache2
+ 
+# Adjust firewall settings to allow traffic on port 80 (HTTP)
+
+sudo ufw allow 'Apache' # could require port destination and source parameters. Could allow all inbound access instead of just allowing Apache.
+ 
+sudo bash -c 'cat > /var/www/html/index.html <<EOF
+<html>
+<head>
+<title>Welcome to My Web Page</title>
+</head>
+<body>
+<h1>Hello, World!</h1>
+<p>This is a simple web page hosted on my Apache server.</p>
+</body>
+</html>
+
+EOF'
+
+ 
--- main.tf
+++ main.tf
@@ -64,3 +64,46 @@ resource "azurerm_public_ip" "atp-ip" {
   location            = var.location
   allocation_method   = "Static"
 }
+
+resource "azurerm_network_interface" "atp-nic" {
+  name                = "atp-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.atp-rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.atp-subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.atp-ip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "atp-vm" {
+  name                = "${var.prefix}vm"
+  resource_group_name = azurerm_resource_group.atp-rg.name
+  location            = var.location
+  size                = "Standard_B2als_v2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.atp-nic.id
+  ]
+
+  custom_data = filebase64("customdata.tpl")
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_ed25519.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
